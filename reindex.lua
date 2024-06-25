ee1db4c2352514fc31f8dad61cb7942d5685e57e
Zooka <136661366+ZookaOnGit@users.noreply.github.com>
2024-06-25 13:58:32 +0400

    Update reindex.lua

--- reindex.lua
+++ reindex.lua
@@ -1,30 +1,31 @@
+-- Requires:
 -- sudo apt install libzzip-dev
--- sudo luarocks install inspect
 -- sudo luarocks install json
 -- sudo luarocks install luazip
 
-local inspect = require "inspect"
 local json = require "JSON"
 local zip = require "zip"
 
 local pkg = {}
 
+-- loop through all .mpackage files in the directory
 for file in io.popen([[ls -pa *.mpackage | grep -v /]]):lines() do
     print("Found "..file)
---    table.insert(pkgs, file)
 
+    -- read config.lua from the zip file
     local zfile, err = zip.open(file)
     local f1, err = zfile:open('config.lua')
     local s1 = f1:read("*a")
+
+    -- output config.lua and run it to gather the variables inside
     infoFile = io.open("config.lua", "w+")
     io.output(infoFile)
     io.write(s1)
     io.close(infoFile)
     dofile("config.lua")
 
+    -- insert package details in table
     table.insert(pkg, { ["mpackage"] = mpackage, ["author"] = author, ["title"] = title, ["description"] = description, ["created"] = created, ["version"] = version} )
-
-    --print(inspect(pkg))
 
     f1:close()
     zfile:close()
@@ -34,8 +35,6 @@ end
 local index = {}
 
 table.insert(index, { ["name"] = "mudlet package repository listing", ["updated"] = os.date("%c"), ["packages"] = pkg } )
-
---print(json:encode(index))
 
 indexFile = io.open("mpkg.packages.json", "w+")
 io.output(indexFile)
