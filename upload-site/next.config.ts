92ae963249132598263f72d55da915c9703e889d
Vadim Peretokin <vadi2@users.noreply.github.com>
2025-01-01 16:23:31 +0100

    WIP

--- upload-site/next.config.ts
+++ upload-site/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // output: 'export',
+  // basePath: '/mudlet-package-repository',
 };
 
 export default nextConfig;
