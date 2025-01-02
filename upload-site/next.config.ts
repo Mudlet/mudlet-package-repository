a293741b2ab30ca32d260c223a7054017a71ed69
Vadim Peretokin <vadi2@users.noreply.github.com>
2025-01-02 17:39:01 +0100

    Reset config options

--- upload-site/next.config.ts
+++ upload-site/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/mudlet-package-repository',
+  /* config options here */
 };
 
 export default nextConfig;
