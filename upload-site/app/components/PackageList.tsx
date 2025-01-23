5a4bf2d252e5ce2db8c0259bb55babf996b730a7
Vadim Peretokin <vperetokin@hey.com>
2025-01-23 08:06:32 +0100

    Links need to be on raw.githubusercontent.com, otherwise github previews them (#218)
    Co-authored-by: Vadim Peretokin <vadi2@users.noreply.github.com>

--- upload-site/app/components/PackageList.tsx
+++ upload-site/app/components/PackageList.tsx
@@ -68,7 +68,7 @@ export const PackageList = ({ packages, limit }: PackageListProps) => {
               {pkg.icon && (
                 <div className="flex-shrink-0">
                   <Image
-                    src={`https://github.com/Mudlet/mudlet-package-repository/blob/refs/heads/main/${pkg.icon}`}
+                    src={`https://raw.githubusercontent.com/Mudlet/mudlet-package-repository/refs/heads/main/${pkg.icon}`}
                     alt={`${pkg.mpackage} icon`}
                     width={48}
                     height={48}
